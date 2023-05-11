704eb3b3c5c9a40339448f6d54f2e9a7b1484a9f
Marco 9726 <marcosarra9@gmail.com>
2023-05-11 12:46:11 +0200

    es1

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,14 +1,27 @@
 package main;
 
+import abs.Animal;
+import animals.Bird;
 import animals.Dog;
+import animals.Dolphin;
+import animals.Eagle;
 
 public class Main {
 	
 	public static void main(String[] args) {
 		
-		Dog d1 = new Dog("bau", "carne");
+		Dog d1 = new Dog("bau", "meat");	
+		Bird b1 = new Bird("chip", "insects");
+		Eagle e1 = new Eagle("cryEagle", "meat");
+		Dolphin do1 = new Dolphin("cryDolphin", "fish");
 		
-		System.out.println(d1);
+		Animal[] animals = { d1, b1, e1, do1 };
+		
+		for ( int i = 0; i < animals.length; i++) {
+			
+			System.out.println(animals[i]);
+			System.out.println("-----------");
+		}
 		
 		
 	}
